aae8496a5faacb3b0e41c4b051924c8b6773e77f
ben44496 <ben44496@gmail.com>
2021-02-14 09:29:47 -0600

    Somethings wrong, but it's Sahil's problem now

--- synthetic_data_gen/main.py
+++ synthetic_data_gen/main.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import learner_type_generator as ltg
+import video_data_generator as vdg
+from operator import itemgetter
 
 if __name__ == "__main__":
     dataset = "learner_feature_data.csv"
     generator = ltg.LearnerTypeGenerator(dataset)
+    vid_gen = vdg.VideoDataGenerator("video_data.csv", "learner_feature_data.csv")
+    print(vid_gen.generate_video_affinity_data(1))
+    vid_gen.update_file()
+    # for i in range(0, 200):
+    #     generator.generate_new_person_data()
+    # generator.update_file()
 
-    for i in range(0, 100):
-        generator.generate_new_person_data()
-    generator.update_file()
+    # print(generator.dot_product(4))
+    # print(max(generator.dot_product(2), key=itemgetter(0))[0])
+    # print(generator.dot_product(3))
+    # print(generator.dot_product(4))
---
+++ synthetic_data_gen/video_data_generator.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy.random as random
+import json
+import learner_type_generator as ltg
+
+class VideoDataGenerator:
+
+    def __init__(self, data_path, learner_feature_data):
+        self.incidents = pd.read_csv(data_path)
+        self.data_path = data_path
+        self.learner_feature_data = learner_feature_data
+
+    def update_file(self):
+        self.incidents.to_csv(self.data_path, mode="w", header=True, index=False)
+
+    def generate_video_affinity_data(self, player_id):
+        with open("../youtube_video/full_data.json") as f:
+            full_data = json.load(f)
+        # feature_dataset = pd.read_csv(learner_feature_data) # gives us their learning style (0, 1, 2)
+        
+        generator = ltg.LearnerTypeGenerator(self.learner_feature_data)
+        video_list = []
+        learning_type = generator.dot_product(player_id)
+
+        for topic in full_data.keys():
+            for i in range(len(full_data[topic])):
+                if i == learning_type:
+                    norm = random.normal(4, 1)
+                    affinity = (norm < 1)*1.0 + (norm >= 2 and norm <=5)*norm + (norm>5)*5.0
+                    
+                    print((player_id, full_data[topic][i]["videoId"], int(affinity)))
+                    video_list.append((player_id, full_data[topic][i]["videoId"], int(affinity)))
+                    break
+            video_index = random.randint(4, 9)
+            denorm = random.normal(1, 3)
+            diffinity = (denorm < 0.0)*0.0 + (denorm >= 0.0 and denorm <= 5.0)*denorm + (denorm>5.0)*5.0
+            video_list.append((player_id, full_data[topic][video_index]["videoId"], int(-1*diffinity)))
+
+        self.incidents = self.incidents.append(video_list, ignore_index=True)
